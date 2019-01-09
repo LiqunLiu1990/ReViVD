e587f31c8507c55400a0e326cd9ab9d5d0daa5aa
yohhaan <yohan.beugin@ecl17.ec-lyon.fr>
2019-01-09 12:01:18 +0100

    docs controllers

---
+++ ReViVD_unity_project/Assets/Controllers.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class Controllers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameObject controller_left = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        controller_left.transform.position = new Vector3(-1, 2, 0);
+
+        GameObject controller_right = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        controller_right.transform.position = new Vector3(1, 2, 0);
+
+        var interactionSourceStates = InteractionManager.GetCurrentReading();
+        foreach (var interactionSourceState in interactionSourceStates) {
+                }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.GetJoystickNames()[0]);
+        Debug.Log(Input.GetJoystickNames()[1]);
+
+    }
+}
---
+++ docs/controllers.md
@@ -0,0 +1,5 @@
+# Controllers
+
+Unity docs for Windows Mixed Reality Input: [https://docs.unity3d.com/Manual/Windows-Mixed-Reality-Input.html 
+https://docs.unity3d.com/Manual/Windows-Mixed-Reality-Input.html]
+
