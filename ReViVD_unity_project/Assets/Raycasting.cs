2858ed23e02a41289355774eee47afc43794be55
yohhaan <yohan.beugin@ecl17.ec-lyon.fr>
2019-01-29 12:20:59 +0100

    raycasting test

---
+++ ReViVD_unity_project/Assets/Raycasting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycasting : MonoBehaviour
+{    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 debut = new Vector3(0, 0, 0);
+        Vector3 fin = new Vector3(5, 5, 5);
+
+        //Ray rayon = new Ray(debut, fin);
+        //Debug.DrawRay(debut, fin, Color.yellow);
+          
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(debut, fin, out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(debut, fin* hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(debut, fin * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+
+
+    }
+}
