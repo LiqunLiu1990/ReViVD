74c715601450e6243aec736b940794a1ae753924
yohhaan <yohan.beugin@ecl17.ec-lyon.fr>
2019-02-05 15:12:10 +0100

    Raycasting on several objects

--- ReViVD_unity_project/Assets/Raycasting.cs
+++ ReViVD_unity_project/Assets/Raycasting.cs
@@ -12,28 +12,38 @@ public class Raycasting : MonoBehaviour
         cubetohit.transform.position = new Vector3(10, 40, 50);
         //cubetohit.AddComponent<MeshCollider>();
 
+
+        GameObject cubetohit2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cubetohit2.transform.position = new Vector3(20, 80, 100);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 start = new Vector3(0, 0, 0);
-        Vector3 end = new Vector3(10,40,50);
+        Vector3 end = new Vector3(20,80,100);
 
-        RaycastHit hit;
-        // Does the ray intersect any objects
-        if (Physics.Raycast(start, end, out hit, Mathf.Infinity))
+        RaycastHit[] hits;
+
+        if (Physics.Raycast(start, end, Mathf.Infinity)) //does the ray intersect at least one object
         {
-            Debug.DrawRay(start, end* hit.distance, Color.yellow);
-            Debug.Log("hit");
-            Debug.Log(hit.point);
+            hits = Physics.RaycastAll(start, end, Mathf.Infinity);
+            Debug.DrawRay(start, end, Color.yellow);
+
+            for (int i = 0; i < hits.Length; i++)
+            {
+                Debug.Log("hit");
+                Debug.Log(hits[i].point);
+            }
         }
-        else 
+        else
         {
             Debug.DrawRay(start, end * 1000, Color.white);
             Debug.Log("Did not Hit");
+
         }
 
-
+     
     }
 }
